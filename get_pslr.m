4355845c06e2b4d596e2166a630bf25e2a9dc2d2
Alex Bradley <alextbradley.1@gmail.com>
2023-02-27 11:53:03 +0000

    Update get_pslr.m
    fit proceedure changed (interp to gaussian)

--- get_pslr.m
+++ get_pslr.m
@@ -30,24 +30,38 @@ function pslr = get_pslr(x, slr, gamma, D, sigma_m, sigma_g, mu)
 
 pslr = zeros(size(x));
 
+% create the fit to P(mit| gamma_x)
+yy = 1/sqrt(2*pi*sigma_m^2) * exp (-D .^2 /2/sigma_m^2);
+dg = diff(gamma);
+dg = dg(1);
+yy = yy/(sum(yy)*dg);
+f = fit(gamma',yy','Gauss1');
 
 % for each x, compute gamma_x (the values of gamma which return slr of x)
 for ix = 1:length(x)
     gamma_x = get_gamma_x(x(ix), slr, gamma);
 
+
     for ig = 1:length(gamma_x)
 
         % for each gamma_x, compute D(gamma_x) and therefore P(mitgcm| gamma_x)
-        D_gammax = interp1(gamma,D,gamma_x(ig)); %use linear interpolation to find the value of D(gamma_x);
-        P_mit_given_gammax = 1/sqrt(2*pi*sigma_m^2) * exp (-D_gammax .^2 /2/sigma_m^2);
-        
+        %D_gammax = interp1(gamma,D,gamma_x(ig)); %use linear interpolation to find the value of D(gamma_x);
+        %P_mit_given_gammax = 1/sqrt(2*pi*sigma_m^2) * exp (-D_gammax .^2 /2/sigma_m^2);
+        P_mit_given_gammax = f(gamma_x);
+
+
         % for each gamma_x, compute P(gamma_x | mu)  
-        P_gammax_given_mu =  1/sqrt(2*pi*sigma_g^2)*exp(-(gamma_x - mu).^2 /2/sigma_g^2);
+        P_gammax_given_mu =  1/sqrt(2*pi*sigma_g^2)*exp(-(gamma_x(ig) - mu).^2 /2/sigma_g^2);
 
         % take the product and add it
         l = P_mit_given_gammax * P_gammax_given_mu;
+
        
+%         try
         pslr(ix) = pslr(ix) + l;
+%         catch
+%             l
+%         end
         
     end
 end
