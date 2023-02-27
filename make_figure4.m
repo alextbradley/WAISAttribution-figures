1c289a9c84363776b95c186118365cf1fbeb9b8e
Alex Bradley <alextbradley.1@gmail.com>
2023-02-27 09:54:17 +0000

    Update make_figure4.m

--- make_figure4.m
+++ make_figure4.m
@@ -2,16 +2,21 @@
 % (a) SLR as a function of time for single ensemble member for each different value of alpha.
 % (b) SLR as a function of alpha at times t1, t2, ...
 % (c) Table of contour plots of melt rate at timeslices for different alpha values. Likelihood values and mean likelihood labelled.
-% (d) Likehood as a function of gamma_T
+% (d) Likehood as a function of gamma_T as a heatmap
 % (e) P(mitgcm|mu, gamma) (for different values of sigma_m?)
 % (f) P(gamma|mu) (for different values of sigma_a)
 % (g) P(SLR = x) as a function of x
+% (nb this script produces (c) last)
+%
+% 24/02/23, ATB (aleey@bas.ac.uk), MIT license
+% 
 
 %
 % Preliminaries
 %
 addpath('plottools')
 gendata = 1; %set to 1 to pass thru the gendata loop
+fs = 13; %plot fontsize
 
 %
 % Constants
@@ -63,7 +68,7 @@ if gendata
     end
 end %end gendata flag
 
-%%
+%% make panels a and b
 % Plot data
 tshow = [20,40,60,80]; %times to show in b
 colmapb = cmocean('haline',length(tshow)+1); %colourmap in b
@@ -126,7 +131,17 @@ for it = 1:length(tshow)
         SLR_at_this_time(ig) = volume_change(idxt) / 395 / 1e9; %SLR in mm
 
     end %end loop over gamma values
+    SLR_times{it} = SLR_at_this_time;
     plot(gammas_act, (SLR_at_this_time),'color', colmapb(it,:), 'linewidth', 2)
+
+
+%     % test the get_gamma_x script
+%     x = 0.2; %text value
+%     gamma_x = get_gamma_x(x, SLR_at_this_time, gammas_act);
+%     plot([min(gammas_act), gamma_x], x*[1,1], '--', 'linewidth', 2)
+%     1
+%     drawnow ; pause
+
 end %end loop over time pts
 
 ax(2).XLabel.String = '$\gamma_T~\times 10^{3}$';
@@ -167,7 +182,7 @@ axs.YTick = [-800,-500,-200];
 axs.YLabel.String = '$P(t;\mathcal{F})$'; 
 axs.YLabel.Interpreter = 'latex';
 
-%% plot c:
+%% generate data for the rest of the plots
 gendata_c = 1; 
 
 nx = 300;
@@ -185,40 +200,151 @@ if gendata
     s_mit =  ss_mit.ss;
     melt_wavi = nan(lg,lt,nx,ny);
     melt_mit  = nan(lg,lt,nx,ny);
+    D = nan(lg,lt);
 
     for it = 1:lt %timeslice value
         for ig = 1:lg %gamma value 
             % get the wavi melt rates
             tt = s(ig,ensemble,member).t;
-            s(ig,ensemble,member).fid
+            s(ig,ensemble,member).fid;
             [~,idx] = min(abs(tt - timeslices(it))); %index of time entry matching
             
-            melt_wavi(ig,it,:,:) = s(ig,ensemble,member).m(:,:,idx);
+            mw = s(ig,ensemble,member).m(:,:,idx); %ice model melt rate
+            melt_wavi(ig,it,:,:) = mw;
 
             %get the mitgcm melt rates
-            melt_mit(ig,it,:,:) = s_mit(ig,ensemble,member,it).m;
+            mm = s_mit(ig,ensemble,member,it).m;
+            melt_mit(ig,it,:,:) = mm;
+
+            %also work out D(ig, it):
+            hh = s(ig,ensemble,member).h(:,:,idx); %ice thickness at this point
+            base = -918/1028 *hh;
+            idx = (base < -600) & (mm ~= 0) & (mw ~=0); %points counted in the comparison
+            dM = mm - mw;
+            D(ig,it) = mean(mean(dM(idx)));
+
         end
     end
 end %end gendata flag
 
-% make plot for this part
-figure(2); clf;
 
-ncols = length(gamma_idx);
-nrows = 2*length(timeslices);
-colgap = 0.02;
-starty = 0.02;
-rowgap = 0.01;
-height = 1/(nrows+2); %height of plot
-width  = 1/(ncols+1); %width of plot
-startx = (1 -width*ncols - (ncols-1)*colgap)/2;
-positions = zeros(4, ncols, nrows);
-for p = 1:nrows
-    for q = 1:ncols
-        positions(:,q,p) = [startx + (q-1)*colgap + (q-1)*width, starty + (p-1)*height + (p-1)*rowgap, width, height];
-       
-    end
+%% make panel d
+figure(4); clf; 
+D = abs(D);
+DD = [D; mean(D)];h = heatmap(abs(DD));
+ax =gca;
+ax.YDisplayLabels = {'0', '25', '50', '75', '100', ''};
+ax.XDisplayLabels = {'8', '9', '10', '11', '12'};
+cl = ax.ColorLimits;
+ax.ColorLimits = [0, cl(2)];
+cmap =  flipud(cmocean('ice',100));
+cmap = cmap(1:end-20,:);
+ax.Colormap = cmap;
+ax.FontName = 'GillSans';
+ax.FontSize = fs;
+fig = gcf;
+fig.Position(3:4) = [500,300];
+
+%% make panel e
+% plot of likelihood contribution from Dbar
+figure(5); clf; hold on; box on;
+Dbar = mean(D);
+
+%create a spline fit to this 
+
+xx = linspace(min(gammas_act), max(gammas_act));
+sigma_m = [1,5,10,25]; 
+
+cmap = flipud(cmocean('amp', length(sigma_m) + 2));
+for ism = 1:length(sigma_m)
+    yy = 1/sqrt(2*pi*sigma_m(ism)^2) * exp (-Dbar .^2 /2/sigma_m(ism)^2);
+
+
+    yy = yy/sum(yy); %normalize (grid spacing is 1)
+
+    f = fit(gammas_act',yy','Gauss1');
+    plot(xx, f(xx), 'linewidth', 2, 'Color',cmap(ism+1,:))
+    %plot(gammas_act, yy,'o', 'markeredgecolor', 'k', 'markerfaceColor',cmap(ism+1,:))
+
 end
+fig = gcf;
+fig.Position(3:4) = [500,300];
+
+ax = gca; 
+ax.FontSize = fs;
+ax.XTick = gammas_act;
+ax.FontName = 'GillSans';
+%ax.YLabel.String = 'density';
+
+%% make panel f
+figure(6); clf; hold on; box on;
+mu = 10;
+sigma_g = [0.5,1, 2.5, 5];
+xx = linspace(min(gammas_act), max(gammas_act));
+cmap = flipud(cmocean('tempo', length(sigma_g)+2));
+dx = diff(xx); dx = dx(1);
+for ig = 1:length(sigma_g)
+    density = 1/sqrt(2*pi*sigma_g(ig)^2)*exp(-(xx - mu).^2 /2/sigma_g(ig)^2);
+    density = density/sum(density) *dx;
+    plot(xx, density, 'linewidth', 2, 'Color',cmap(ig+1,:));
+end
+fig = gcf;
+fig.Position(3:4) = [500,300];
+
+ax = gca; 
+ax.FontSize = fs;
+ax.XTick = gammas_act;
+ax.FontName = 'GillSans';
+
+%% make panel g
+figure(7); clf; hold on; box on
+x = linspace(0,4);
+dx = diff(x);
+dx = dx(1);
+
+%choose constnats
+sigma_m = 5;
+sigma_g = 1;
+mu = 10;
+
+for it = 1:length(tshow)
+
+    slr = cell2mat(SLR_times(it));
+    pslr = get_pslr(x, slr, gammas_act, Dbar, sigma_m, sigma_g, mu);
+    pslr = pslr / (sum(pslr)*dx);
+
+    plot(x, pslr, 'linewidth', 2, 'Color', colmapb(it,:));
+end 
+
+
+fig = gcf;
+fig.Position(3:4) = [500,300];
+
+ax = gca; 
+ax.FontSize = fs;
+ax.XTick = 0:3;
+ax.XLim = [0,3];
+ax.FontName = 'GillSans';
+
+%% make panel c 
+% 
+figure(3); clf;
+
+% ncols = length(gamma_idx);
+% nrows = 2*length(timeslices);
+% colgap = 0.02;
+% starty = 0.02;
+% rowgap = 0.01;
+% height = 1/(nrows+2); %height of plot
+% width  = 1/(ncols+1); %width of plot
+% startx = (1 -width*ncols - (ncols-1)*colgap)/2;
+% positions = zeros(4, ncols, nrows);
+% for p = 1:nrows
+%     for q = 1:ncols
+%         positions(:,q,p) = [startx + (q-1)*colgap + (q-1)*width, starty + (p-1)*height + (p-1)*rowgap, width, height];
+%        
+%     end
+% end
 
 count = 1;
 for p = 1:length(timeslices)
