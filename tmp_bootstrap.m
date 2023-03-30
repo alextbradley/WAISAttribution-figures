81226dfe52e775115e0b994a42d3af15144f2ec3
Alex Bradley <alextbradley.1@gmail.com>
2023-03-30 08:39:04 +0100

    Create tmp_bootstrap.m

---
+++ tmp_bootstrap.m
@@ -0,0 +1,36 @@
+
+nboot = 1000;
+
+anth_ci_upper = nan(length(t), length(x));
+anth_ci_lower = nan(length(t), length(x));
+nat_ci_upper  = nan(length(t), length(x));
+nat_ci_lower  = nan(length(t), length(x));
+for it = 1:length(t)
+    tic
+    for ix = 1:length(xx)
+
+        %anthro
+        vals = squeeze(anth_all_subsamp(it,:,ix));
+        ci = bootci(nboot,@mean,vals);
+        anth_ci_upper(it,ix) = ci(1);
+        anth_ci_lower(it,ix) = ci(2);
+
+        %counterfactual
+        vals = squeeze(nat_all_subsamp(it,:,ix));
+        ci = bootci(nboot,@mean,vals);
+        nat_ci_upper(it,ix) = ci(1);
+        nat_ci_lower(it,ix) = ci(2);
+        
+        if mod(ix,100) == 0
+            ix
+        end
+
+    end
+    toc
+    it
+end
+
+
+
+figure(1); clf; 
+p = imagesc(xx, t, log10(ci_upper));
